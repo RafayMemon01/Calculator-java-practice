f401cd6b88c66c5146bcbdcb4b416358cfd6a9e6
RafayMemon01 <rafayrashid200611@gmail.com>
2023-02-18 20:33:43 +0500

    Calculator was done

---
+++ java.js
@@ -0,0 +1,21 @@
+// let num1 = prompt("enter your first number: ");
+//         let num2 = prompt("enter your second number: ");
+//         // "10"
+//         // "12"
+
+
+//         num1 = Number(num1); // 10
+//         num2 = Number(num2); // 12
+
+//         let num3 = num1 + num2;
+
+//         alert("Sum is: " + num3);
+
+        
+
+let num1 = prompt("Enter Your 1st Number");        
+let num2 = prompt("Enter Your 2nd Number"); 
+num1 = Number (num1);       
+num2 = Number (num2); 
+let num3 = num1 + num2;
+alert("Answer is: " + num3);      
